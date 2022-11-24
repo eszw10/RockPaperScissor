459ea730a7fc24fd2b07f0b040216f612bf6d107
eszw10 <erycszw10@gmail.com>
2022-11-24 07:58:22 +0700

    add some logic to the script

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let choices = ['rock','paper','scissors']
 let player = 0;
 let comp = 0;
+let scoreUsr = document.getElementById('user')
+let ai = document.getElementById('comp')
+let result = document.getElementById('result')
+let btn = document.querySelectorAll('button')
+
 function getComputerChoice() {
     return choices[Math.floor(Math.random()*3)]
 }
@@ -9,42 +14,71 @@ function playRound(playerSelection, computerSelection) {
     if(playerSelection == 'rock') {
         if(computerSelection == 'paper') {
             comp+=1
-            return 'Computers Win! Paper beat Rock'
+            scoreUsr.textContent = `User score : ${player}`
+            ai.textContent = `Computer score : ${comp}`
+            result.textContent = 'Computers Win! Paper beat Rock';
         } else if(computerSelection == 'scissors') {
             player+=1
-            return 'Player Win Rock beat Scissors!'
+            scoreUsr.textContent = `User score : ${player}`
+            ai.textContent = `Computer score : ${comp}`
+            result.textContent = 'Player Win Rock beat Scissors!'
         }
     } else if(playerSelection == 'paper') {
         if(computerSelection == 'scissors') {
             comp+=1
-            return 'Computers Win Scissors beat Paper'
+            scoreUsr.textContent = `User score : ${player}`
+            ai.textContent = `Computer score : ${comp}`
+            result.textContent = 'Computers Win Scissors beat Paper'
         } else if(computerSelection == 'rock') {
             player+=1
-            return 'Player Win! Paper beat Rock'
+            scoreUsr.textContent = `User score : ${player}`
+            ai.textContent = `Computer score : ${comp}`
+            result.textContent = 'Player Win! Paper beat Rock'
         }
     } else if(playerSelection == 'scissors') {
         if(computerSelection == 'rock') {
             comp+=1
-            return 'Computers Win! Rock beat Scissors'
+            scoreUsr.textContent = `User score : ${player}`
+            ai.textContent = `Computer score : ${comp}`
+            result.textContent = 'Computers Win! Rock beat Scissors'
         } else if(computerSelection == 'paper') {
             player+=1
-            return 'Player Win! Scissors beat Paper'
+            scoreUsr.textContent = `User score : ${player}`
+            ai.textContent = `Computer score : ${comp}`
+            result.textContent ='Player Win! Scissors beat Paper'
         }
+    } else if(playerSelection == computerSelection){
+        result.textContent = "It's a tie!"
     } else {
-        return "It's a tie!"
+        return
     }
+    checkGame();
 }
 
-let userInput = '';
-function game() {
-    for(let i = 0;i<=4;i++) {
-        userInput = prompt('Take your weapons! ').toLowerCase();
-        console.log(playRound(userInput,getComputerChoice()));
+
+btn.forEach(button => {
+    button.addEventListener('click', ()=> {
+        if(button.id == 'rock') {
+            playRound('rock',getComputerChoice())
+        } else if(button.id == 'paper') {
+            playRound('paper',getComputerChoice())
+        } else if(button.id == 'scissors') {
+            playRound('scissors', getComputerChoice())
+        }
     }
-    if(player > comp) {
-        console.log('Player is the winner!')
-    } else {
-        console.log('Computer is the winner!')
+)})
+function checkGame() {
+    if(comp == 5 || player == 5) {
+        document.getElementById('container').removeChild(result)
+        if(comp > player) {
+            let hasilGame = document.createElement('p')
+            hasilGame.textContent = 'The Winner is Computer!'
+            document.getElementById('container').appendChild(hasilGame)
+        } else {
+            let hasilGame = document.createElement('p')
+            hasilGame.textContent = 'The Winner is Player!'
+            document.getElementById('container').appendChild(hasilGame)
+        }
     }
-}
-game();
+};
+console.log(comp)
